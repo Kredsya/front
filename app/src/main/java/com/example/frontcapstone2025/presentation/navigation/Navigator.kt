4bd5321763ead9351f6a1127325d0288f1d4e66d
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2025-05-23 03:03:58 +0900

    feat : GetAllDistancePage UI 구현

--- app/src/main/java/com/example/frontcapstone2025/presentation/navigation/Navigator.kt
+++ app/src/main/java/com/example/frontcapstone2025/presentation/navigation/Navigator.kt
@@ -3,16 +3,22 @@ package com.example.frontcapstone2025.presentation.navigation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.frontcapstone2025.presentation.screen.GetAllDistancePage
 import com.example.frontcapstone2025.presentation.screen.GetArmLengthPage
 import com.example.frontcapstone2025.presentation.screen.LoadingPage
 import com.example.frontcapstone2025.presentation.screen.MainPage
 import com.example.frontcapstone2025.presentation.screen.SearchWifiPage
 import com.example.frontcapstone2025.presentation.screen.SettingPage
+import com.example.frontcapstone2025.presentation.screen.WifiListPage
 
 fun navivationWithClear(navController: NavController, route: String) {
     navController.popBackStack()
@@ -25,22 +31,28 @@ fun Navigator() {
 
     val navigationBack: () -> Unit = { navController.navigateUp() }
 
+    var centerButtonTarget by rememberSaveable { mutableStateOf("MainPage") }
     val bottomBaronClickedActions = listOf(
         { navivationWithClear(navController = navController, route = "SearchWifiPage") },
-        { navivationWithClear(navController = navController, route = "MainPage") },
+        {
+            navivationWithClear(navController, centerButtonTarget) // 상태 기반으로 이동
+        },
         { navivationWithClear(navController = navController, route = "SettingPage") },
     )
 
     Scaffold { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = "MainPage",
+            startDestination = "GetAllDistancePage",
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(route = "MainPage") {
                 MainPage(
                     bottomBaronClickedActions = bottomBaronClickedActions,
-                    moveToLoadingPage = { navController.navigate("LoadingPage") }
+                    moveToLoadingPage = {
+                        navController.navigate("LoadingPage")
+                        centerButtonTarget = "WifiListPage"
+                    }
                 )
             }
             composable(route = "SettingPage") {
@@ -53,12 +65,31 @@ fun Navigator() {
                 )
             }
 
+            composable(route = "WifiListPage") {
+                WifiListPage(
+                    bottomBaronClickedActions = bottomBaronClickedActions,
+                    moveToFirstMainPage = {
+                        centerButtonTarget = "MainPage"
+                        navController.navigate("MainPage")
+                    }
+                )
+            }
+
             composable(route = "GetArmLengthPage") {
-                GetArmLengthPage(navigationBack = navigationBack)
+                GetArmLengthPage(
+                    navigationBack = navigationBack,
+                    moveToGetAllDistancePage = { navController.navigate("GetAllDistancePage") }
+                )
+            }
+            composable(route = "GetAllDistancePage") {
+                GetAllDistancePage(navigationBack = navigationBack)
             }
 
             composable(route = "LoadingPage") {
-                LoadingPage(text = "주변 네트워크를 검색하고 있어요.\n대략 30~40초 정도 걸려요.")
+                LoadingPage(
+                    text = "주변 네트워크를 검색하고 있어요.\n대략 30~40초 정도 걸려요.",
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/example/frontcapstone2025/presentation/screen/GetAllDistancePage.kt
@@ -0,0 +1,206 @@
+package com.example.frontcapstone2025.presentation.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.frontcapstone2025.R
+import com.example.frontcapstone2025.components.layout.TopBarWithBack
+import com.example.frontcapstone2025.ui.theme.TextColorGray
+
+@Composable
+fun GetAllDistancePage(
+    navigationBack: () -> Unit
+) {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        topBar = { TopBarWithBack(navigationBack = navigationBack) },
+
+        ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(horizontal = 24.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFFB4C5AB))
+                    .padding(16.dp)
+            ) {
+                Column {
+                    Text(
+                        text = "※ 주의",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 24.sp,
+                        color = TextColorGray,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+
+                    Text(
+                        text = buildAnnotatedString {
+                            append("1. ")
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append("왼팔이 자유롭게")
+                            }
+                            append(" 움직일 수 있는 위치로 이동 후, 결과를 얻을 때까지 ")
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append("움직이지 마세요.")
+                            }
+                        },
+                        fontSize = 16.sp,
+                        lineHeight = 20.sp,
+                        color = TextColorGray
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = buildAnnotatedString {
+                            append("2. ")
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Red,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append("핸드폰을 왼손")
+                            }
+                            append("에 쥐고, 사진의 동작을 따라해주세요.")
+                        },
+                        fontSize = 16.sp,
+                        lineHeight = 20.sp,
+                        color = TextColorGray
+                    )
+                }
+
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.up),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(196.dp)
+                            .clickable { }
+                    )
+                    Text(
+                        text = "측정: O",
+                        modifier = Modifier,
+                        color = TextColorGray
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.down),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(196.dp)
+                            .clickable { }
+                    )
+                    Text(
+                        text = "측정: X",
+                        modifier = Modifier,
+                        color = TextColorGray
+
+                    )
+                }
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.left),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(196.dp)
+                            .clickable { }
+                    )
+                    Text(
+                        text = "측정: O",
+                        modifier = Modifier,
+                        color = TextColorGray
+                    )
+                }
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.weight(0.5f)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.front),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .height(196.dp)
+                            .clickable { }
+                    )
+                    Text(
+                        text = "측정: X",
+                        modifier = Modifier,
+                        color = TextColorGray
+
+                    )
+                }
+            }
+
+
+        }
+
+
+    }
+}
